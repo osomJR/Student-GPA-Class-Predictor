f15b5733d9d44a54bc56b74c2db6debf2b3a6fef
Akan Osom <awesomeosom10@gmail.com>
2026-01-28 06:06:20 +0100

    update on frontend/script.js

--- frontend/script.js
+++ frontend/script.js
@@ -19,7 +19,7 @@ document
     };
 
     try {
-      const response = await fetch("/predict", {
+      const response = await fetch("http://127.0.0.1:5000/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
